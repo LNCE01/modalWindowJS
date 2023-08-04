54616db0799b819dc2aa6f83f5ead1cadadf6a1b
Nicolas Clerici Erhardt <nicolasclericierhardt@gmail.com>
2023-08-05 03:39:55 +1000

    renaming, testing parameters off the utils

--- script.js
+++ script.js
@@ -2,23 +2,21 @@
 import closeModalWindow from "./src/utils/closeWindow.js";
 import openModalWindow from "./src/utils/openWindow.js";
 
-const modal = document.querySelector(".modal");
-const overlay = document.querySelector(".overlay");
 const btnCloseModal = document.querySelector(".close-modal");
 const btnsOpenModal = document.querySelectorAll(".show-modal");
 
 //Looping through the 3 buttons (which is basically an array holding all the buttons)
 for (let i = 0; i < btnsOpenModal.length; i++) {
   //we input a function expression
-  btnsOpenModal[i].addEventListener("click", openModalWindow(modal, overlay));
+  btnsOpenModal[i].addEventListener("click", openModalWindow);
 }
 
-btnCloseModal.addEventListener("click", closeModalWindow(modal, overlay));
+btnCloseModal.addEventListener("click", closeModalWindow);
 
-overlay.addEventListener("click", closeModalWindow(modal, overlay));
+overlay.addEventListener("click", closeModalWindow);
 
 document.addEventListener("keydown", function (event) {
   if (event.key === "Escape" && !modal.classList.contains("hidden")) {
-    closeModalWindow(modal, overlay);
+    closeModalWindow();
   }
 });
