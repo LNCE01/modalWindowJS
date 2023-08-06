89a6373b7617da95272d83cfa6438fe1246693a5
Nicolas Clerici Erhardt <nicolasclericierhardt@gmail.com>
2023-08-07 03:47:28 +1000

    fixed bug, erased parameters on close window file

--- src/utils/closeWindow.js
+++ src/utils/closeWindow.js
@@ -1,5 +1,5 @@
 //Function to close modal window so that we can pass it to several event listeners (btnCloseModal, overlay)
-const closeModalWindow = function (modal, overlay) {
+const closeModalWindow = function () {
   document.querySelector(".modal").classList.add("hidden");
   document.querySelector(".overlay").classList.add("hidden");
 };
