a0714f372fc24c1e32564cb0092481c9927e0cd0
Nicolas Clerici Erhardt <nicolasclericierhardt@gmail.com>
2023-08-07 03:48:40 +1000

    erased comments on closeWindow file

--- src/utils/closeWindow.js
+++ src/utils/closeWindow.js
@@ -1,4 +1,3 @@
-//Function to close modal window so that we can pass it to several event listeners (btnCloseModal, overlay)
 const closeModalWindow = function () {
   document.querySelector(".modal").classList.add("hidden");
   document.querySelector(".overlay").classList.add("hidden");
